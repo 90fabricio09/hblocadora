55514ebaed7d282e4901ed00b4ec45eecf4a377e
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2025-09-17 09:59:48 -0300

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,14 +20,6 @@ const Footer = () => {
                         </p>
                     </Col>
                     <Col md={4}>
-                        <h5 className="mb-3 text-white">Serviços</h5>
-                        <ul className="list-unstyled">
-                            <li className="text-white"><i className="bi bi-chevron-right me-2"></i>Transporte Executivo</li>
-                            <li className="text-white"><i className="bi bi-chevron-right me-2"></i>Transporte de Passageiros</li>
-                            <li className="text-white"><i className="bi bi-chevron-right me-2"></i>Transfer Aeroporto</li>
-                        </ul>
-                    </Col>
-                    <Col md={4}>
                         <h5 className="mb-3 text-white">Contato</h5>
                         <p className="text-white">
                             <i className="bi bi-telephone me-2"></i>
@@ -53,6 +45,21 @@ const Footer = () => {
                                 São Paulo - SP, 04109-120
                             </a>
                         </p>
+                    </Col>
+                    <Col md={4}>
+                        <h5 className="mb-3 text-white">Localização</h5>
+                        <div className="map-container">
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.8010616457777!2d-46.63687552479453!3d-23.575587378790033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce599ef8771b69%3A0xb7ab51ef55012de8!2sHB%20Locadora!5e0!3m2!1spt-BR!2sbr!4v1758113595070!5m2!1spt-BR!2sbr"
+                                width="100%"
+                                height="200"
+                                style={{ border: 0, borderRadius: '8px' }}
+                                allowFullScreen=""
+                                loading="lazy"
+                                referrerPolicy="no-referrer-when-downgrade"
+                                title="Localização HB Locadora"
+                            ></iframe>
+                        </div>
                     </Col>
                 </Row>
                 <hr className="my-4" />
